de7da81774a155e4d81d353e4b7c3084eeb06ad6
liaoen <2229314974@qq.com>
2022-10-23 15:50:05 +0800

    first commit

---
+++ src/main/com/example/test/Test.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.refresh();
+    }
+}
